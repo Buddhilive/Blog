1d7332c469e00693eba0a59c2d6fd835c17e23d1
Buddhi Kavindra <visitbudkavin@gmail.com>
2023-02-19 20:12:01 +0530

    adding nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,25 +11,8 @@ import { Component, OnInit } from '@angular/core';
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+        <li class="nav-item" *ngFor="let item of navItems">
+          <a class="nav-link active" aria-current="page" [routerLink]="item.link">{{item.name}}</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -44,6 +27,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class NavComponent implements OnInit {
+
+  navItems = [
+    {
+      name: 'Home',
+      link: ''
+    }
+  ];
 
   constructor() { }
 
